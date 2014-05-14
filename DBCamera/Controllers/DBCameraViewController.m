cba088dc61a89f96c0588e34333c0ee18957d467
myeyesareblind <myeyesareblind@gmail.com>
2014-05-14 20:00:44 +0300

    DBCameraViewController # account for custom camera view

--- DBCamera/Controllers/DBCameraViewController.m
+++ DBCamera/Controllers/DBCameraViewController.m
@@ -170,6 +170,11 @@ NSLocalizedStringFromTable(key, @"DBCamera", nil)
 
 - (DBCameraView *) cameraView
 {
+    if ( _customCamera )
+    {
+        return _customCamera;
+    }
+    
     if ( !_cameraView ) {
         _cameraView = [DBCameraView initWithCaptureSession:self.cameraManager.captureSession];
 //        [_cameraView setHideLibraryButton:![NSStringFromClass(self.parentViewController.class) isEqualToString:@"DBCameraContainer"]];
