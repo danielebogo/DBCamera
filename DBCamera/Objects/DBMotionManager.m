5b5bad3695327845aeec4b4917d7ffb5b4e33be9
Aleksi Hoffman <aleksi@healthpuzzle.me>
2014-08-05 17:19:34 +0300

    Fixed typedef for DBMotionManagerRotationHandler block, added check that motionRotationHandler exists before trying to call it

--- DBCamera/Objects/DBMotionManager.m
+++ DBCamera/Objects/DBMotionManager.m
@@ -101,7 +101,10 @@
 - (void) deviceOrientationDidChangeTo:(UIDeviceOrientation)orientation
 {
     [self setLastOrientation:orientation];
-    self.motionRotationHandler(self.lastOrientation);
+
+    if (self.motionRotationHandler) {
+        self.motionRotationHandler(self.lastOrientation);
+    }
 }
 
 @end
