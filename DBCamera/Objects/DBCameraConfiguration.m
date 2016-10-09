82381d51f23cab764db4984445bc67cc51dec4b5
Nikita Tuk <654070@gmail.com>
2016-10-09 18:30:24 +0500

    Added camera configuration object & protocol for processing controller

---
+++ DBCamera/Objects/DBCameraConfiguration.m
@@ -0,0 +1,15 @@
+//
+//  DBCameraConfiguration.m
+//  DBCamera
+//
+//  Created by Nikita Tuk on 09/10/16.
+//  Copyright © 2016 PSSD - Daniele Bogo. All rights reserved.
+//
+
+#import "DBCameraConfiguration.h"
+
+@implementation DBCameraConfiguration
+
+
+
+@end
---
+++ DBCamera/Objects/DBPhotoProcessingControllerProtocol.h
@@ -0,0 +1,13 @@
+//
+//  DBPhotoProcessingControllerProtocol.h
+//  DBCamera
+//
+//  Created by Nikita Tuk on 09/10/16.
+//  Copyright © 2016 PSSD - Daniele Bogo. All rights reserved.
+//
+
+@protocol DBPhotoProcessingControllerProtocol <NSObject>
+
+@property (nonatomic, nonnull, strong, readonly) UIButton *cropButton;
+
+@end
